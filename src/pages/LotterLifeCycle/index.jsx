0ef78f81a27029de4c01221ff0c91b443340f879
haris-75 <haris.ejaz@datumbrain.com>
2023-08-13 15:51:48 +0500

    Added LifeCycle Arrows

--- src/pages/LotterLifeCycle/index.jsx
+++ src/pages/LotterLifeCycle/index.jsx
@@ -3,16 +3,17 @@ import list from '../../assets/list.png';
 import jackpot from '../../assets/jackpot.png';
 import booking from '../../assets/booking.png';
 import ImageMarquee from './ImageMarquee';
+import arrow from '../../assets/arrow.png';
 
 const LotteryPhaseCard = ({ imageSrc, title, text, number, positionClass }) => (
-  <div className='flex flex-col items-center lg:gap-5 md:gap-3 gap-2 relative shadow-card p-8 rounded-3xl w-fit'>
+  <div className='z-20 flex flex-col items-center lg:gap-5 md:gap-3 gap-2 relative shadow-card p-8 rounded-3xl w-fit bg-white'>
     <div>
       <img
         src={imageSrc}
         alt={`${title}-img`}
       />
     </div>
-    <h1 className='z-10 font-semibold lg:text-2xl md:text-xl text-lg capitalize'>
+    <h1 className='z-10 font-semibold lg:text-2xl md-lg:text-xl text-lg capitalize'>
       {title}
     </h1>
     <p className='z-10 font-light md:text-sm text-xs xl:max-w-[300px] lg:max-w-[250px] max-w-[200px]'>
@@ -41,7 +42,7 @@ export default function LotteryLifeCycle() {
             containing Lorem Ipsum passages.
           </p>
         </div>
-        <div className='flex flex-col md:flex-row w-full justify-between gap-5 items-center'>
+        <div className='flex flex-col md:flex-row w-full justify-between md:gap-10 gap-5 items-center relative'>
           <LotteryPhaseCard
             imageSrc={list}
             title='choose number'
@@ -53,7 +54,7 @@ export default function LotteryLifeCycle() {
           />
           <LotteryPhaseCard
             imageSrc={booking}
-            title='choose number'
+            title='buy ticket'
             text='It has survived not only five centuries, but also the leap into
             electronic typesetting, remaining essentially unchanged. It was
             popularised in the 1960s with the release of Letraset sheets.'
@@ -62,13 +63,31 @@ export default function LotteryLifeCycle() {
           />
           <LotteryPhaseCard
             imageSrc={jackpot}
-            title='choose number'
+            title='win jackpot'
             text='It has survived not only five centuries, but also the leap into
             electronic typesetting, remaining essentially unchanged. It was
             popularised in the 1960s with the release of Letraset sheets.'
             number={'03'}
             positionClass='-bottom-[45px] -right-[11px]'
           />
+          <div className='absolute w-full md:h-[50px] lg:[60px] xl:[75px] bg-primary hidden md:block'>
+            <div className='flex justify-between md:w-[39%] lg:w-[38%] xl:w-[37%] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+              <div>
+                <img
+                  width={30}
+                  src={arrow}
+                  alt='arrow'
+                />
+              </div>
+              <div>
+                <img
+                  width={30}
+                  src={arrow}
+                  alt='arrow'
+                />
+              </div>
+            </div>
+          </div>
         </div>
         <ImageMarquee />
       </div>
